a659f0bf689f250ca532067062ce3e5b0ad86af4
Richard Condori <sis719richardcondori@gmail.com>
2023-12-07 23:22:39 -0400

    Exercise N5 Destructuring

---
+++ destructuring/Destructuring.js
@@ -0,0 +1,20 @@
+const person = {
+    head: {
+        eyes: '8',
+        mouth: {
+            teeth:'7',
+            tongue: '4'
+        }
+    },
+    body: {
+        shoulders: '10',
+        chest: '9',
+        arms: '8.5',
+        hands: '9',
+        legs: '10'
+    }
+};
+let myEyes=person.head.eyes;
+let myLegs=person.body.legs;
+console.log(myLegs)
+console.log(myEyes)
